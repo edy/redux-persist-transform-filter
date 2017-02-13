92760842d859187df8ab5748f9bc924f21f54ae2
Eduard Baun <eduard@baun.de>
2017-02-13 19:02:00 +0100

    Release 0.0.9

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,9 @@
 Object.defineProperty(exports, "__esModule", {
 	value: true
 });
+exports.default = createFilter;
+exports.createWhitelistFilter = createWhitelistFilter;
+exports.createBlacklistFilter = createBlacklistFilter;
 exports.persistFilter = persistFilter;
 
 var _reduxPersist = require('redux-persist');
@@ -15,23 +18,38 @@ var _lodash3 = require('lodash.set');
 
 var _lodash4 = _interopRequireDefault(_lodash3);
 
+var _lodash5 = require('lodash.unset');
+
+var _lodash6 = _interopRequireDefault(_lodash5);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = function (reducerName, inboundPaths, outboundPaths) {
+function createFilter(reducerName, inboundPaths, outboundPaths) {
+	var transformType = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : 'whitelist';
+
 	return (0, _reduxPersist.createTransform)(
 	// inbound
 	function (inboundState, key) {
-		return inboundPaths ? persistFilter(inboundState, inboundPaths) : inboundState;
+		return inboundPaths ? persistFilter(inboundState, inboundPaths, transformType) : inboundState;
 	},
 
 	// outbound
 	function (outboundState, key) {
-		return outboundPaths ? persistFilter(outboundState, outboundPaths) : outboundState;
-	}, { whitelist: [reducerName] });
+		return outboundPaths ? persistFilter(outboundState, outboundPaths, transformType) : outboundState;
+	}, { 'whitelist': [reducerName] });
 };
 
+function createWhitelistFilter(reducerName, inboundPaths, outboundPaths) {
+	return createFilter(reducerName, inboundPaths, outboundPaths, 'whitelist');
+}
+
+function createBlacklistFilter(reducerName, inboundPaths, outboundPaths) {
+	return createFilter(reducerName, inboundPaths, outboundPaths, 'blacklist');
+}
+
 function persistFilter(state) {
-	var paths = arguments.length <= 1 || arguments[1] === undefined ? [] : arguments[1];
+	var paths = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : [];
+	var transformType = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : 'whitelist';
 
 	var subset = {};
 
@@ -40,13 +58,26 @@ function persistFilter(state) {
 		paths = [paths];
 	}
 
-	paths.forEach(function (path) {
-		var value = (0, _lodash2.default)(state, path);
+	if (transformType === 'whitelist') {
+		paths.forEach(function (path) {
+			var value = (0, _lodash2.default)(state, path);
 
-		if (typeof value !== 'undefined') {
-			(0, _lodash4.default)(subset, path, value);
-		}
-	});
+			if (typeof value !== 'undefined') {
+				(0, _lodash4.default)(subset, path, value);
+			}
+		});
+	} else if (transformType === 'blacklist') {
+		subset = Object.assign({}, state);
+		paths.forEach(function (path) {
+			var value = (0, _lodash2.default)(state, path);
+
+			if (typeof value !== 'undefined') {
+				(0, _lodash6.default)(subset, path);
+			}
+		});
+	} else {
+		subset = state;
+	}
 
 	return subset;
 }
