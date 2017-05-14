df8136135c83e54ba041d3afa00a8ebbf9c07f16
Iacopo <iacopo.pazzaglia@hotmail.it>
2017-05-14 23:07:36 +0200

    Store only the items that matches a predicate

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import { createTransform } from 'redux-persist';
 import get from 'lodash.get';
 import set from 'lodash.set';
 import unset from 'lodash.unset';
+import pickBy from 'lodash.pickby';
+import isEmpty from 'lodash.isempty';
 
 export default function createFilter (reducerName, inboundPaths, outboundPaths, transformType = 'whitelist') {
 	return createTransform(
@@ -31,6 +33,11 @@ export function createBlacklistFilter (reducerName, inboundPaths, outboundPaths)
 	return createFilter(reducerName, inboundPaths, outboundPaths, 'blacklist');
 }
 
+function filterObject({ path, filterFunction = () => true }, state) {
+  const value = get(state, path);
+  return pickBy(value, filterFunction);
+}
+
 export function persistFilter (state, paths = [], transformType = 'whitelist') {
 	let subset = {};
 
@@ -41,20 +48,36 @@ export function persistFilter (state, paths = [], transformType = 'whitelist') {
 
 	if (transformType === 'whitelist') {
 		paths.forEach((path) => {
-			const value = get(state, path);
+			if (typeof path === 'object') {
+				const value = filterObject(path, state);
 
-			if (typeof value !== 'undefined') {
-				set(subset, path, value);
+				if (!isEmpty(value)) {
+					set(subset, path.path, value);
+				}
+			} else {
+				const value = get(state, path);
+
+				if (typeof value !== 'undefined') {
+					set(subset, path, value);
+				}
 			}
 		});
 	} else if (transformType === 'blacklist') {
 		subset = Object.assign({}, state);
 		paths.forEach((path) => {
-			const value = get(state, path);
+			if (typeof path === 'object') {
+				const value = filterObject(path, state);
 
-			if (typeof value !== 'undefined') {
-				unset(subset, path);
-			}
+				if (!isEmpty(value)) {
+					unset(subset, path.path);
+				}
+			} else {
+				const value = get(state, path);
+
+				if (typeof value !== 'undefined') {
+					unset(subset, path);
+				}
+		}
 		});
 	} else {
 		subset = state;
